66d304f60ab6d6217ed99e9e16e83e6e2fa48714
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-01-29 15:19:04 -0600

    Add files via upload

---
+++ Jan29.js
@@ -0,0 +1,32 @@
+console.log('Lifting weights repetition 1 ッ');
+console.log('Lifting weights repetition 2 ッ');
+console.log('Lifting weights repetition 3 ッ');
+console.log('Lifting weights repetition 4 ッ');
+console.log('Lifting weights repetition 5 ッ');
+console.log('Lifting weights repetition 6 ッ');
+console.log('Lifting weights repetition 7 ッ');
+console.log('Lifting weights repetition 8 ッ');
+console.log('Lifting weights repetition 9 ッ');
+console.log('Lifting weights repetition 10 ッ');
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifiting weights reptition ${rep} ッ`);
+}
+
+
+
+//////////////////////////////////////////////////
+
+console.log('Hai')
+for (let 1 = 0; 1 < jaz.length; 1++) {
+    if (typeof jaz[1] !== 'string') continue;
+
+    console.log(jazz[1], typeof jaz[1]));
+}
+
+console.log('Hai')
+for (let 1 = 0; 1 < jaz.length; 1++) {
+    if (typeof jaz[1] !== 'number') break;
+
+    console.log(jazz[1], typeof jaz[1]));
+}
